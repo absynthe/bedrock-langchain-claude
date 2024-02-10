a16250d0649cf1f4d36371615912ac62309d68b0
Anamaria Todor <anamarto@amazon.com>
2024-02-10 13:18:11 +0100

    Modified F-3 to use Claude.

--- completed/langchain/bedrock_langchain.py
+++ completed/langchain/bedrock_langchain.py
@@ -1,19 +1,18 @@
 import os
 from langchain.llms.bedrock import Bedrock
 
+#sets the profile name to use for AWS credentials
+os.environ["AWS_PROFILE"] = "bedrock-ana" #replace with your profile name
+
 ##################################################################
 
-llm = Bedrock( #create a Bedrock llm client
-    credentials_profile_name=os.environ.get("BWB_PROFILE_NAME"), #sets the profile name to use for AWS credentials (if not the default)
-    region_name=os.environ.get("BWB_REGION_NAME"), #sets the region name (if not the default)
-    endpoint_url=os.environ.get("BWB_ENDPOINT_URL"), #sets the endpoint URL (if necessary)
-    model_id="ai21.j2-ultra-v1" #set the foundation model
+llm = Bedrock(
+    model_id="anthropic.claude-v2" #set the foundation model
 )
 
 ##################################################################
 
 prompt = "What is the largest city in Vermont?"
-    
-response_text = llm.predict(prompt) #return a response to the prompt
+response_text = llm.invoke(prompt) #return a response to the prompt
 
 print(response_text)
